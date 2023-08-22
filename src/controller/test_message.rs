d9f20f25b411e34b56136a02d11ada2e3a2ee5c8
Alexander Makedonski <alexandermakedonski@Alexanders-MacBook-Pro.local>
2023-08-22 09:47:25 +0300

    add controllers

--- src/controller/test_message.rs
+++ src/controller/test_message.rs
@@ -1,13 +1,17 @@
-use crate::{error::Error, configuration::{AppState, State}};
-use actix_web::{get, web, HttpResponse, Result};
+use crate::{
+    configuration::{AppState, State},
+    error::Error,
+};
+use actix_web::{get, web, Responder, Result};
 use serde::{Deserialize, Serialize};
 
 #[get("/message")]
-async fn index(state: web::Data<AppState<State>>) -> Result<HttpResponse, Error> {
-    Ok(HttpResponse::Ok().into())
+async fn index(state: web::Data<AppState<State>>) -> Result<impl Responder, Error> {
+    let (block,) = state.database.block.get_first_block().await?;
+    Ok(web::Json(TestMessag { block }))
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct TestMessag {
-    pub test: bool,
+    pub block: i64,
 }
