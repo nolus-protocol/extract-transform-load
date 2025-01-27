f5509bd5da1e34870920a7e260e0af0b540c2ede
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2025-01-27 19:20:07 +0200

    refactor(dao/postgre): Extract SQL into constants, apply some easier query optimizations and use `Zero::zero` instead of parsing.

--- src/dao/postgre/ls_loan_closing.rs
+++ src/dao/postgre/ls_loan_closing.rs
@@ -1,140 +1,121 @@
-use std::str::FromStr as _;
-
-use bigdecimal::BigDecimal;
+use bigdecimal::{BigDecimal, Zero as _};
 use sqlx::{Error, Transaction};
 
 use crate::model::{LS_Loan_Closing, Pnl_Result, Realized_Pnl_Result, Table};
 
-use super::{DataBase, QueryResult};
+use super::DataBase;
 
 impl Table<LS_Loan_Closing> {
-    pub async fn isExists(
-        &self,
-        contract: String,
-    ) -> Result<bool, crate::error::Error> {
-        let (value,): (i64,) = sqlx::query_as(
-            r#"
-            SELECT 
-                COUNT(*)
-            FROM "LS_Loan_Closing" 
-            WHERE 
-                "LS_contract_id" = $1
-            "#,
+    pub async fn isExists(&self, contract: String) -> Result<bool, Error> {
+        const SQL: &str = r#"
+        SELECT EXISTS(
+            SELECT
+            FROM "LS_Loan_Closing"
+            WHERE "LS_contract_id" = $1
         )
-        .bind(contract)
-        .fetch_one(&self.pool)
-        .await?;
+        "#;
 
-        if value > 0 {
-            return Ok(true);
-        }
-
-        Ok(false)
+        sqlx::query_as(SQL)
+            .bind(contract)
+            .fetch_one(&self.pool)
+            .await
+            .map(|(result,)| result)
     }
 
     pub async fn insert(
         &self,
         data: LS_Loan_Closing,
         transaction: &mut Transaction<'_, DataBase>,
-    ) -> Result<QueryResult, Error> {
-        sqlx::query(
-            r#"
-            INSERT INTO "LS_Loan_Closing" (
-                "LS_contract_id",
-                "LS_amnt",
-                "LS_amnt_stable",
-                "LS_pnl",
-                "LS_timestamp",
-                "Type",
-                "Block",
-                "Active"
-            )
-            VALUES($1, $2, $3, $4, $5, $6, $7, $8)
-        "#,
+    ) -> Result<(), Error> {
+        const SQL: &str = r#"
+        INSERT INTO "LS_Loan_Closing" (
+            "LS_contract_id",
+            "LS_amnt",
+            "LS_amnt_stable",
+            "LS_pnl",
+            "LS_timestamp",
+            "Type",
+            "Block",
+            "Active"
         )
-        .bind(&data.LS_contract_id)
-        .bind(&data.LS_amnt)
-        .bind(&data.LS_amnt_stable)
-        .bind(&data.LS_pnl)
-        .bind(&data.LS_timestamp)
-        .bind(&data.Type)
-        .bind(&data.Block)
-        .bind(&data.Active)
-        .execute(&mut **transaction)
-        .await
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        "#;
+
+        sqlx::query(SQL)
+            .bind(&data.LS_contract_id)
+            .bind(&data.LS_amnt)
+            .bind(&data.LS_amnt_stable)
+            .bind(&data.LS_pnl)
+            .bind(data.LS_timestamp)
+            .bind(&data.Type)
+            .bind(data.Block)
+            .bind(data.Active)
+            .execute(&mut **transaction)
+            .await
+            .map(drop)
     }
 
     pub async fn get_lease_amount(
         &self,
         contract_id: String,
-    ) -> Result<BigDecimal, crate::error::Error> {
-        let value: (Option<BigDecimal>,) = sqlx::query_as(
-            r#"
-                SELECT SUM("Amount") as "Total" FROM (
-                    SELECT
-                    SUM("LS_loan_amnt") as "Amount"
-                    FROM "LS_Opening"
-                    WHERE "LS_contract_id" = $1
-                UNION ALL
-                    SELECT
-                    -SUM("LS_amnt") as "Amount"
-                    FROM "LS_Close_Position"
-                    WHERE "LS_contract_id" = $1
-                UNION ALL
-                    SELECT
-                    -SUM("LS_amnt") as "Amount"
-                    FROM "LS_Liquidation"
-                    WHERE "LS_contract_id" = $1
-                ) AS combined_data
-            "#,
-        )
-        .bind(contract_id)
-        .fetch_one(&self.pool)
-        .await?;
-        let (amnt,) = value;
-        let amnt = amnt.unwrap_or(BigDecimal::from_str("0")?);
+    ) -> Result<BigDecimal, Error> {
+        const SQL: &str = r#"
+        SELECT (
+            (
+                SELECT SUM("LS_loan_amnt")
+                FROM "LS_Opening"
+                WHERE "LS_contract_id" = $1
+            ) - (
+                SELECT SUM("LS_amnt")
+                FROM "LS_Close_Position"
+                WHERE "LS_contract_id" = $1
+            ) - (
+                SELECT SUM("LS_amnt")
+                FROM "LS_Liquidation"
+                WHERE "LS_contract_id" = $1
+            )
+        ) AS "Total"
+        "#;
 
-        Ok(amnt)
+        sqlx::query_as(SQL)
+            .bind(contract_id)
+            .fetch_optional(&self.pool)
+            .await
+            .map(|f| f.map_or_else(BigDecimal::zero, |(result,)| result))
     }
 
-    pub async fn update(
-        &self,
-        data: LS_Loan_Closing,
-    ) -> Result<QueryResult, Error> {
-        sqlx::query(
-            r#"
-            UPDATE 
-                "LS_Loan_Closing"
-            SET
-                "LS_amnt" = $1,
-                "LS_amnt_stable" = $2,
-                "LS_pnl" = $3,
-                "Active" = $4
-            WHERE 
-                "LS_contract_id" = $5
+    pub async fn update(&self, data: LS_Loan_Closing) -> Result<(), Error> {
+        const SQL: &str = r#"
+        UPDATE "LS_Loan_Closing"
+        SET
+            "LS_amnt" = $1,
+            "LS_amnt_stable" = $2,
+            "LS_pnl" = $3,
+            "Active" = $4
+        WHERE "LS_contract_id" = $5
+        "#;
 
-        "#,
-        )
-        .bind(&data.LS_amnt)
-        .bind(&data.LS_amnt_stable)
-        .bind(&data.LS_pnl)
-        .bind(&data.Active)
-        .bind(&data.LS_contract_id)
-        .execute(&self.pool)
-        .await
+        sqlx::query(SQL)
+            .bind(&data.LS_amnt)
+            .bind(&data.LS_amnt_stable)
+            .bind(&data.LS_pnl)
+            .bind(data.Active)
+            .bind(&data.LS_contract_id)
+            .execute(&self.pool)
+            .await
+            .map(drop)
     }
 
     pub async fn get_leases_to_proceed(
         &self,
     ) -> Result<Vec<LS_Loan_Closing>, Error> {
-        let data = sqlx::query_as(
-            r#"
-            SELECT * FROM "LS_Loan_Closing" WHERE "Active" = false;
-            "#,
-        )
-        .fetch_all(&self.pool)
-        .await?;
-        Ok(data)
+        const SQL: &str = r#"
+        SELECT *
+        FROM "LS_Loan_Closing"
+        WHERE "Active" = false
+        "#;
+
+        sqlx::query_as(SQL).fetch_all(&self.pool).await
     }
 
     pub async fn get_leases(
@@ -143,69 +124,52 @@ impl Table<LS_Loan_Closing> {
         skip: i64,
         limit: i64,
     ) -> Result<Vec<Pnl_Result>, Error> {
-        let data = sqlx::query_as(
-            r#"
-            SELECT 
-                "LS_Loan_Closing"."LS_contract_id",
-                "LS_Loan_Closing"."LS_amnt",
-                "LS_Loan_Closing"."LS_amnt_stable",
-                "LS_Loan_Closing"."LS_pnl",
-                "LS_Loan_Closing"."LS_timestamp",
-                "LS_Loan_Closing"."Type",
-                "LS_Loan_Closing"."Block",
-                "LS_Opening"."LS_asset_symbol",
-                "LS_Opening"."LS_loan_pool_id"
-            FROM 
-                "LS_Loan_Closing" 
-            INNER JOIN 
-                "LS_Opening" 
-            ON 
-                "LS_Loan_Closing"."LS_contract_id" = "LS_Opening"."LS_contract_id"  
-            WHERE 
-                "LS_Loan_Closing"."Active" = true
-            AND
-                "LS_Opening"."LS_address_id" = $1
-            ORDER BY 
-               "LS_Loan_Closing"."LS_timestamp" 
-            DESC 
-            OFFSET 
-                $2 
-            LIMIT 
-                $3;
-            "#,
-        )
-        .bind(address)
-        .bind(skip)
-        .bind(limit)
-        .fetch_all(&self.pool)
-        .await?;
-        Ok(data)
+        const SQL: &str = r#"
+        SELECT
+            "loan_closing"."LS_contract_id",
+            "loan_closing"."LS_amnt",
+            "loan_closing"."LS_amnt_stable",
+            "loan_closing"."LS_pnl",
+            "loan_closing"."LS_timestamp",
+            "loan_closing"."Type",
+            "loan_closing"."Block",
+            "opening"."LS_asset_symbol",
+            "opening"."LS_loan_pool_id"
+        FROM "LS_Loan_Closing" AS "loan_closing"
+        JOIN "LS_Opening" AS "opening" ON "loan_closing"."LS_contract_id" = "opening"."LS_contract_id"
+        WHERE
+            "loan_closing"."Active" = true AND
+            "opening"."LS_address_id" = $1
+        ORDER BY "closing"."LS_timestamp" DESC
+        OFFSET $2
+        LIMIT $3
+        "#;
+
+        sqlx::query_as(SQL)
+            .bind(address)
+            .bind(skip)
+            .bind(limit)
+            .fetch_all(&self.pool)
+            .await
     }
 
     pub async fn get_realized_pnl(
         &self,
         address: String,
-    ) -> Result<Vec<Realized_Pnl_Result>, crate::error::Error> {
-        let data= sqlx::query_as(
-            r#"
-                SELECT
-                    s."LS_pnl", o."LS_loan_pool_id", o."LS_asset_symbol"
-                FROM "LS_Loan_Closing"  s
-                LEFT JOIN "LS_Opening" o ON o."LS_contract_id" = s."LS_contract_id"
-                WHERE s."LS_contract_id" IN 
-                (
-                    SELECT
-                        "LS_contract_id"
-                    FROM "LS_Opening"
-                    WHERE
-                        "LS_address_id" = $1
-                )
-            "#,
-        )
-        .bind(address)
-        .fetch_all(&self.pool)
-        .await?;
+    ) -> Result<Vec<Realized_Pnl_Result>, Error> {
+        const SQL: &str = r#"
+        SELECT
+            "loan_closing"."LS_pnl",
+            "opening"."LS_loan_pool_id",
+            "opening"."LS_asset_symbol"
+        FROM "LS_Loan_Closing" AS "loan_closing"
+        JOIN "LS_Opening" AS "opening" ON "loan_closing"."LS_contract_id" = "opening"."LS_contract_id"
+        WHERE "opening"."LS_address_id" = $1
+        "#;
 
-        Ok(data)
+        sqlx::query_as(SQL)
+            .bind(address)
+            .fetch_all(&self.pool)
+            .await
     }
 }
