1a60cbad6ea64f41fdc1235a6f6f80cce03c1e04
Kiril Mihaylov <80464733+KirilMihaylov@users.noreply.github.com>
2025-01-29 17:41:29 +0200

    refactor: Explicitly specify the pattern to be tuple of unit types to prevent possible future mistakes.

--- src/main.rs
+++ src/main.rs
@@ -61,7 +61,7 @@ async fn app_main() -> Result<(), Error> {
     mp_assets::fetch_insert(app_state.clone(), None).await?;
     let mut event_manager = Event::new(app_state.clone());
 
-    let (_, _, _, _, _) = tokio::try_join!(
+    let ((), (), (), (), ()) = tokio::try_join!(
         event_manager.run(),
         mp_assets::mp_assets_task(app_state.clone()),
         start_aggregation_tasks(app_state.clone()),
