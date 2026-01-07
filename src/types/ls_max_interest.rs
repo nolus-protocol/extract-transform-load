008eca1fb86e97c49c9c7732ae194682f636b832
kostovster <kostovster@gmail.com>
2026-01-07 13:02:10 +0200

    refactor: remove deprecated legacy endpoints
    Remove legacy endpoints that are no longer in use:
    - /api/max_ls_interest_7d/{pool_id}: Legacy max lease interest endpoint
    - /api/max_lp_ratio/{pool_id}: Legacy max LP ratio endpoint
    
    Deleted files:
    - src/controller/max_lp_ratio.rs
    - src/controller/max_ls_interest_7d.rs
    - src/types/ls_max_interest.rs
    - src/types/max_lp_ratio.rs
    
    Updated module registrations in:
    - src/controller/mod.rs
    - src/types/mod.rs
    - src/server.rs
    
    This cleanup removes unused code and simplifies the API surface.

--- src/types/ls_max_interest.rs
+++
@@ -1,9 +0,0 @@
-use chrono::NaiveDate;
-use serde::{Deserialize, Serialize};
-use sqlx::prelude::FromRow;
-
-#[derive(Debug, FromRow, Deserialize, Serialize)]
-pub struct LS_Max_Interest {
-    pub date: NaiveDate,
-    pub max_interest: i16,
-}
